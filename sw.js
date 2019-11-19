e307c846c27190316e881dc9bd4b61ade400b965
Oliversinn <https://Oliversinn@github.com>
2019-11-18 20:53:20 -0600

    ya es una PWA

--- sw.js
+++ sw.js
@@ -1,25 +1,34 @@
-var cacheName = 'hello-pwa';
-var filesToCache = [
+var urlsToCache = [
   '/',
   '/index.html',
   '/css/style.css',
   '/js/main.js'
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-first-cache-v1')
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  )
+})
+
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  )
+})
